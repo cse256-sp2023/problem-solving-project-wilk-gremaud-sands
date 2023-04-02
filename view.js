a46837440932b81b639fe56b7c3fae6eb1ba2e79
gremaudm <89869019+gremaudm@users.noreply.github.com>
2023-04-02 14:46:47 -0500

    Update view.js

--- view.js
+++ view.js
@@ -72,3 +72,4 @@ $('.permbutton').click( function( e ) {
 
 // ---- Assign unique ids to everything that doesn't have an ID ----
 $('#html-loc').find('*').uniqueId() 
+$('.permbutton').append('Permissions')
